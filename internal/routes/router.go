0f135fa560cf0da7cee4730c7a2952e57bac01ec
Thang Nguyen <mrwin95@gmail.com>
2024-10-05 00:43:12 -0600

    refactor route move to controller

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -1,9 +1,8 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/go-ecommerce-backend-api/internal/controller"
 )
 
 func NewRouter() *gin.Engine {
@@ -13,25 +12,8 @@ func NewRouter() *gin.Engine {
 
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/ping", Pong)
-		v1.PUT("/ping", Pong)
-		v1.POST("/ping", Pong)
-		v1.PATCH("/ping", Pong)
+		v1.GET("/ping", controller.Pong)
+		v1.GET("/user/1", controller.GetUserById)
 	}
-
-	v2 := r.Group("/v2")
-	{
-		v2.GET("/ping", Pong)
-		v2.PUT("/ping", Pong)
-		v2.POST("/ping", Pong)
-		v2.PATCH("/ping", Pong)
-	}
-
 	return r
-}
-
-func Pong(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
 }
